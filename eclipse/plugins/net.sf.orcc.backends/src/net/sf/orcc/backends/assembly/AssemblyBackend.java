417b3818766ab4e1c774f28b9bcae1031979b1bd
Burak Bardak <burakbardak@gmail.com>
2014-09-17 13:11:39 +0100

    Assembly Backend added...

---
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/assembly/AssemblyBackend.java
@@ -0,0 +1,13 @@
+package net.sf.orcc.backends.assembly;
+
+import net.sf.orcc.backends.AbstractBackend;
+
+public class AssemblyBackend extends AbstractBackend {
+
+	@Override
+	protected void doInitializeOptions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
